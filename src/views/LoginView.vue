922f1307356edc7cf41e625f5bf47b69b309012d
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-18 10:38:55 +0100

    CSS-Klassen ergänzen.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,12 +1,12 @@
 <template>
 
-  <div class="grid grid-cols-1 md:grid-cols-12 min-h-screen">
+  <div class="LoginView grid grid-cols-1 md:grid-cols-12 min-h-screen">
 
-    <div class="md:col-span-7 bg-gray-50 dark:bg-gray-900 p-4">
+    <div class="LoginView_logo md:col-span-7 p-4">
       <img src="../assets/images/logo/logo.svg" alt="Planner Logo" class="h-screen w-full" />
     </div>
 
-    <div class="md:col-span-5 p-4">
+    <div class="LoginView_form md:col-span-5 p-4">
       <!-- LoginForm.vue Start -->
         <LoginForm />
       <!-- LoginForm.vue Stop -->
