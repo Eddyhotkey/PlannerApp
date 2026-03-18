7626b09508ad5ef31caaf091105605cff1687acb
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-18 07:57:58 +0100

    Mainlayout für die Seiten erstellen und konfigurieren.

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <AppHeader />
+    <div class="flex flex-1">
+      <AppSidebar />
+      <main class="flex-1 p-4">
+        <router-view />
+      </main>
+    </div>
+    <AppFooter />
+  </div>
+</template>
+<script setup>
+import AppHeader from "@/components/layout/AppHeader.vue";
+import AppSidebar from "@/components/layout/AppSidebar.vue";
+import AppFooter from "@/components/layout/AppFooter.vue";
+</script>
+<style scoped>
+
+</style>
