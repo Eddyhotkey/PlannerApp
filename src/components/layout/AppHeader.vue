712ca73a1507e66a499d1aee53aa14628d4073eb
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-18 21:13:58 +0100

    Die Navigation-Componente hinzufügen.

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,8 +1,13 @@
 <template>
-  <h1>Header</h1>
+  <div class="AppHeader flex align-middle justify-between">
+    <DayNavigator />
+    <PeriodNavigator />
+  </div>
 </template>
 <script setup>
 
+import DayNavigator from "@/components/navigation/DayNavigator.vue";
+import PeriodNavigator from "@/components/navigation/PeriodNavigator.vue";
 </script>
 
 <style scoped>
