541b83d900d94691642efca9ebfb643f05a59312
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-18 13:56:33 +0100

    UserBadge hinzufügen und ergänzen.

--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -1,9 +1,15 @@
 <template>
+  <UserBadge
+    name="Rashid Bairamov"
+    subtitle="Administrator"
+    image="../src/assets/images/users/profile-picture-5.jpg"
+  />
   <AppNav />
 </template>
 
 <script setup>
   import AppNav from "@/components/navigation/AppNav.vue";
+  import UserBadge from "@/components/user/UserBadge.vue";
 </script>
 
 <style scoped>
