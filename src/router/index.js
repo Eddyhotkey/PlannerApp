0f9f0a74a7f38b0518c06b8dd2aca5d3ba44828b
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-18 08:06:09 +0100

    Routing für die Navigation konfigurieren.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
+import LoginView from '@/views/LoginView.vue'
 import DayView from "@/views/DayView.vue";
+import MainLayout from "@/layouts/MainLayout.vue";
+import WeekView from "@/views/WeekView.vue";
+import MonthView from "@/views/MonthView.vue";
+import YearView from "@/views/YearView.vue";
 
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routes = [
     {
       path: '/',
       redirect: '/login',
@@ -16,11 +17,48 @@ const router = createRouter({
       component: LoginView,
     },
     {
-      path: '/day',
-      name: 'day',
-      component: DayView,
+      path: '/',
+      component: MainLayout,
+      children: [
+        {
+          path: 'day',
+          name: 'day',
+          component: DayView,
+          meta: { label: 'Tag', menu: true }
+        },
+        {
+          path: 'week',
+          name: 'week',
+          component: WeekView,
+          meta: {
+            label: 'Woche',
+            menu: true,
+          }
+        },
+        {
+          path: 'month',
+          name: 'month',
+          component: MonthView,
+          meta: {
+            label:'Monat',
+            menu: true,
+          }
+        },
+        {
+          path: 'year',
+          name: 'year',
+          component: YearView,
+          meta: {
+            label: 'Jahr',
+            menu: true,
+          }
+        }
+      ],
     }
-  ],
+]
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
 })
 
 export default router
