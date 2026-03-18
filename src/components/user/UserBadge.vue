8277fe7ba884b0afb33b6ab89436daff05d33350
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-18 13:58:17 +0100

    UserBadge: Die Component für Profil-Card in AppAside erstellen und konfigurieren.

---
+++ src/components/user/UserBadge.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <div class="UserBadge mb-5 flex items-center gap-2.5">
+    <img
+      class="w-10 h-10 rounded-full"
+      :src="image"
+      :alt="name">
+    <div class="font-medium text-heading">
+      <p>{{ name }}</p>
+      <p class="text-sm font-normal text-body">{{ subtitle }}</p>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    default: '',
+  },
+  image: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
