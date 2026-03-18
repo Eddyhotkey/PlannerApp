7b4ebe376b312538a40c9a72c60316d87790e2e5
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-18 21:06:56 +0100

    Die Header-Navigation Componente erstellen und configurieren.

---
+++ src/components/navigation/DayNavigator.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="AppHeader_day" >
+    <div class="inline-flex rounded-base shadow-xs -space-x-px" role="group">
+      <button type="button"
+              class="cursor-pointer inline-flex items-center justify-center text-body bg-neutral-primary-soft rounded-s-base box-border border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-3 focus:ring-neutral-tertiary leading-5 px-3 py-2 focus:outline-none">
+        <IconArrowLeft />
+      </button>
+      <p
+        class="text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-3 focus:ring-neutral-tertiary-soft font-medium leading-5 text-sm px-3 py-2 focus:outline-none">
+        16 Montag
+      </p>
+      <button type="button"
+              class="cursor-pointer inline-flex items-center justify-center text-body bg-neutral-primary-soft rounded-e-base box-border border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-3 focus:ring-neutral-tertiary leading-5 px-3 py-2 focus:outline-none">
+        <IconArrowRight />
+      </button>
+    </div>
+  </div>
+</template>
+<script setup >
+
+import IconArrowLeft from "@/components/icons/IconArrowLeft.vue";
+import IconArrowRight from "@/components/icons/IconArrowRight.vue";
+</script>
+<style scoped >
+
+</style>
---
+++ src/components/navigation/PeriodNavigator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="AppHeader_wmy">
+    <div class="inline-flex rounded-base" role="group">
+      <button type="button"
+              class="cursor-pointer text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary-soft shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+      >
+        W 12
+      </button>
+      <button type="button"
+              class="cursor-pointer mx-1 text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary-soft shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+      >
+        März
+      </button>
+      <button type="button"
+              class="cursor-pointer text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary-soft shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+      >
+        2026
+      </button>
+    </div>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>
