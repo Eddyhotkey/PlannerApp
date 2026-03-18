578daa1e3e942fa9e904fd6af91e128ebe9c8f2f
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-18 07:59:22 +0100

    Templates für Jahr, Monat und die Woche erstellen.

---
+++ src/views/MonthView.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "MonthView",
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped >
+
+</style>
---
+++ src/views/YearView.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "YearVue",
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped >
+
+</style>
